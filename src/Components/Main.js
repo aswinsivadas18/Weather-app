ab23dc58a8e88405128554bd9ce6b2f3b14aa4d2
aswinsivadas18 <aswinsivadas18@gmail.com>
2023-06-12 02:30:05 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import Weather from './Weather'
+
+const Main = () => {
+    return (
+        <div>
+            <Weather />
+        </div>
+    )
+}
+export default Main
